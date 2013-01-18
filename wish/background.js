ae663e7ef34e195579045ae659a67285dae6ac12
Kevin <takearest118@gmail.com>
2013-01-18 16:17:30 +0900

    add Wish notification function
    show notification count of Wish at browser icon

--- wish/background.js
+++ wish/background.js
@@ -9,3 +9,25 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 		}
 	}
 });
+
+function checkNotification() {
+	var api = 'http://www.wish.com/api/search';		// TODO it needs to exchange api for notification count
+	var interval = 60;		// seconds
+
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState != 4) return;
+		//if(xhr.status != 200) return;				// TODO must be 400 error code
+		window.setInterval(checkNotification, interval * 1000);
+		var results = JSON.parse(xhr.response);
+		if(results.noti_count == undefined) {
+			chrome.browserAction.setBadgeText({text: '0'});
+		}else {
+			chrome.browserAction.setBadgeText({text: results.noti_count+''});
+		}
+	}
+	xhr.open("GET", api, true);
+	xhr.send();
+}
+
+checkNotification();
